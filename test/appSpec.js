9acfd4aedc4672b67ec50c21e005a76097019b15
Will <will@coefficientinc.com>
2014-08-20 08:19:09 -0400

    Trying to get karma to work

---
+++ test/appSpec.js
@@ -0,0 +1,8 @@
+/* global describe, it, expect, greet */
+
+describe('greeter', function() {
+
+    it('should say Hello to the World', function() {
+        expect(greet('World')).toEqual('Hello, World!');
+    });
+});
