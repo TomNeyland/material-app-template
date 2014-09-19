6e76e525dc6c82e71d2d7b059e63673d4f1351fd
Will <will.soto9@gmail.com>
2014-09-19 08:28:47 -0400

    Move all tasks back into gruntfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,155 @@
 /* global __dirname */
-
 'use strict';
 
 var gulp = require('gulp');
-
 var $ = require('gulp-load-plugins')();
-var _ = require('lodash');
-var args = require('yargs').argv;
-// until gulp 4.0
-var runSequence = require('run-sequence');
+var tagVersion = require('gulp-tag-version');
+
 var karma = require('karma').server;
 
-var config = require('./tasks/config');
+// var config = require('./tasks/config');
 // var karmaCommonConf = require('./karma.conf');
+var config = {
+    app: 'app',
+    build: 'build',
 
-var loadTasks = function(path) {
-    var glob = require('glob');
-    var object = {};
-    var key;
+    server: {
+        port: 8000,
+        url: 'http://localhost:'
+    },
 
-    glob.sync('*', {
-        cwd: path
-    }).forEach(function(option) {
-        require(path + option)(gulp, $, args);
-    });
+    files: {
+        html: [
+            'app/**/*.html',
+            '!app/bower_components/**/*.html',
+            '!index.html'
+        ],
+
+        js: [
+            'gulpfile.js',
+            'app/**/*.js',
+            '!app/bower_components/**/*.js',
+            '!app/templates.js'
+        ],
+
+        jsTests: [
+            'gulpfile.js',
+            'app/**/*.js',
+            '!app/bower_components/**/*.js',
+            'app/tests/**/*.js'
+        ],
+
+        sass: {
+            files: [
+                'app/**/*.scss',
+                '!app/bower_components/**/*.scss'
+            ],
+            src: 'app/app.scss',
+            devDest: 'app/app.css',
+            buildDest: 'build/app.css'
+        },
+
+        ignore: [
+            'app/tests/**/*.js',
+            'app/bower_components/**'
+        ]
+    }
 };
 
-loadTasks('./tasks/options/');
+var release = function(importance) {
+    gulp.src(['./bower.json', './package.json'])
+        .pipe($.bump({
+            type: importance
+        }))
+        .pipe(gulp.dest('./'))
+        .pipe($.git.commit('bumps package version'))
+        .pipe($.filter('bower.json'))
+        .pipe(tagVersion());
+};
+
+gulp.task('clean', function(cb) {
+    require('del')([config.build], {
+        force: true
+    }, cb);
+});
+
+gulp.task('jshint', function() {
+    gulp.src(config.files.js)
+        .pipe($.plumber())
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('connect', function() {
+    var connect = require('connect');
+    var serveStatic = require('serve-static');
+    var app = connect()
+        .use(serveStatic(config.example));
+
+    require('http').createServer(app)
+        .listen(config.server.port)
+        .on('listening', function() {
+            console.log('Started connect web server on ' + config.server.url + config.server.port);
+        });
+});
+
+gulp.task('copy', function() {
+    gulp.src(config.appDir + '**/*.html')
+        .pipe(gulp.dest(config.build));
+});
+
+gulp.task('open', function() {
+    var url = config.server.url + config.server.port;
+
+    require('opn')(url);
+});
+
+gulp.task('convert', function() {
+    gulp.src(config.app + 'bower_components/**/*.css')
+        .pipe($.rename({
+            extname: '.copy.scss'
+        }))
+        .pipe(gulp.dest(config.app + 'bower_components/'));
+});
+
+gulp.task('scss-dev', ['convert'], function() {
+    gulp.src(config.app + '/app.scss')
+        .pipe($.sass({
+            sourceComments: 'map',
+            sourceMap: 'sass'
+        }))
+        .pipe($.autoprefixer('last 2 versions'))
+        .pipe(gulp.dest(config.app))
+        .pipe($.connect.reload());
+});
+
+gulp.task('watch', function() {
+    $.watch(config.scss.files, function(files, cb) {
+        gulp.start('scss-dev', cb);
+    });
+});
+
+gulp.task('requirejs', function() {
+    $.requirejs({
+        mainConfigFile: config.app + '/config.js',
+        baseUrl: config.app,
+        name: 'app',
+        out: 'app.js',
+        useStrict: true,
+        optimizeCss: 'none',
+        generateSourceMaps: false,
+        optimize: 'uglify2',
+        preserveLicenseComments: true
+    })
+        .pipe(gulp.dest(config.buildDir));
+});
+
+gulp.task('test', function(done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true
+    }, done);
+});
 
 gulp.task('default', [
     'connect',
@@ -36,27 +159,16 @@ gulp.task('default', [
     'watch'
 ]);
 
-gulp.task('test', function (done) {
-    karma.start({
-        configFile: __dirname + '/karma.conf.js',
-        singleRun: true
-    }, done);
+gulp.task('build', []);
+
+gulp.task('patch', ['build'], function() {
+    return release('patch');
 });
 
-// gulp.task('default', function(done) {
-//     runSequence('convert', 'sass-dev', 'connect', 'open', 'watch');
-// });
-
-// gulp.task('build', [
-//     'clean',
-//     'copy',
-//     'requirejs'
-// ]);
-
-gulp.task('build', function(done) {
-    runSequence('clean', 'copy', 'requirejs', 'convert', 'sass-build');
+gulp.task('feature', ['build'], function() {
+    return release('minor');
 });
 
 gulp.task('release', ['build'], function() {
-    gulp.task('bump');
+    return release('major');
 });
