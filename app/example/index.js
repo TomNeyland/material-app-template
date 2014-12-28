51619175e4a43bbea7da045c03a0d179a5a7e636
Will <will.soto9@gmail.com>
2014-12-28 12:57:03 -0500

    bugfix(example): fix import

--- app/example/index.js
+++ app/example/index.js
@@ -1,7 +1,7 @@
 import * as angular from 'angular';
 import 'angular-ui-router';
 
-import default as ExampleState from './example';
+import ExampleState from './example';
 
 angular.module('example', [
     'ui.router'
