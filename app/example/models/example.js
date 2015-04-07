aa3763cdd61344c68ff3d2991fded1af117b6aa8
Will <will.soto9@gmail.com>
2015-04-07 08:31:23 -0400

    feat(decorators): Add example decorator

--- app/example/models/example.js
+++ app/example/models/example.js
@@ -1,9 +1,22 @@
 import _ from 'lodash';
 
+import {memoize} from 'common/decorators';
+
 class Foo {
     constructor(data) {
-        _(this).extend(_.omit(data, '$$hashkey'));
+        _.extend(this, _.omit(data, '$$hashKey'));
+    }
+
+    @memoize
+    get name() {
+        return `${this.first} ${this.last}`;
+    }
+
+    set name(val) {
+        let [first, last] = val.split(' ');
+        this.first = first;
+        this.last = last;
     }
 }
 
-export { Foo };
+export default Foo;
